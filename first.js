adf4298fc079d567297965e62b02314a64745cd6
showribabu <kantashowribabu.com>
2023-07-25 08:25:09 +0530

    Basic java script files

---
+++ first.js
@@ -0,0 +1,5 @@
+console.log('hello world')
+
+const add=(a,b)=>{return a+b;}
+const sum=add(1,2);
+console.log(sum);
---
+++ second.js
@@ -0,0 +1,4 @@
+//need to import from another .js file..
+
+require("./first.js") //require () will load ...
+console.log('another')
